0882d0dfacac18e90098658bbd7af2420fe456e7
Johan Sundén <johan.sunden@infomaker.se>
2016-10-14 10:26:22 +0200

    Updated server

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/*
+|--------------------------------------------------------------------------
+| Server.js
+|--------------------------------------------------------------------------
+|
+| This is your local server. Kickstart it by running npm server and it will serve your plugin so that you can install it
+| on dev.dashboard.infomaker.io.
+|
+*/
+
 'use strict'
 
 const express = require('express')
@@ -16,8 +26,6 @@ app.use((req, res, next) => {
 app.use(cors())
 
 app.use(express.static(__dirname+'/build'))
-
-console.log("SERVER PORT", process.env.SERVER_PORT)
 
 http.listen(process.env.SERVER_PORT, () => {
   console.log("\n🎉  " + manifest.name + " can now be installed from http://localhost:" + process.env.SERVER_PORT + "\n")
