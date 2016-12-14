a94ac806ebb784f8681649202067b2ca4184d85b
Johan Sundén <johan.sunden@infomaker.se>
2016-12-14 09:55:49 +0100

    Added Dashboard and React as externals

--- webpack.config.js
+++ webpack.config.js
@@ -7,89 +7,81 @@
 |
 */
 
-const path = require('path');
-const webpack = require('webpack');
-const manifest = require('./manifest.json');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const autoprefixer = require('autoprefixer');
+const path = require('path')
+const webpack = require('webpack')
+const manifest = require('./manifest.json')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const autoprefixer = require('autoprefixer')
 
 console.log("\n ----------------------------")
 console.log(" Plugin development build ")
 console.log(" ----------------------------\n")
 
 module.exports = {
-    entry: "./src/js/main.js",
-    output: {
-        filename: "index.js",
-        path: "build",
-    },
-    devServer: {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": "true"
-      },
-      historyApiFallback: true,
-      inline: true,
-      compress: false,
-      port: 3000
-    },
-    postcss: [
-        autoprefixer({
-            browsers: ['last 2 versions']
-        })
-    ],
-    module: {
-        loaders: [
-          {
-            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-            loader: 'url-loader?limit=100000'
-          },
-          {
-              test: /\.scss$/,
-              loader: ExtractTextPlugin.extract('style', 'css!postcss!sass')
-          },
-          {
-              test: /\.jsx?$/,
-              exclude: /(node_modules)/,
-              loaders: [
-                  'babel?presets[]=stage-0,presets[]=react,presets[]=es2015'
-              ]
-          }
-        ],
-        preLoaders: [
-          { test: /\.jsx?$/, loader: 'eslint', exclude: /node_modules/ },
-          {
-            test: /index\.jsx$/,
-            loader: 'string-replace',
-            query: {
-              multiple: [
-                 { search: '@plugin_bundle', replace: manifest.bundle }
-              ]
-            }
-          }
-        ]
-    },
-    cssLoader: {
-        // True enables local scoped css
-        modules: false,
-        // Which loaders should be applied to @imported resources (How many after the css loader)
-        importLoaders: 1,
-        sourceMap: true
-    },
-    eslint: {
-        failOnWarning: false,
-        failOnError: true
-    },
-    plugins: [
-        new ExtractTextPlugin("style.css"),
-        new webpack.DefinePlugin({
-          'process.env': {
-            'NODE_ENV': JSON.stringify('development')
-          }
-        }),
-        new CopyWebpackPlugin([
-            { from: 'manifest.json', to: '.' }
-        ])
-    ]
-};
+		entry: "./src/js/main.js",
+		output: {
+			filename: "index.js",
+			path: "build",
+		},
+		externals: [
+			"Dashboard",
+			"React"
+		],
+		postcss: [
+			autoprefixer({
+				browsers: ['last 2 versions']
+			})
+		],
+		module: {
+			loaders: [
+				{
+					test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+					loader: 'url-loader?limit=100000'
+				},
+				{
+					test: /\.scss$/,
+					loader: ExtractTextPlugin.extract('style', 'css!postcss!sass')
+				},
+				{
+					test: /\.jsx?$/,
+					exclude: /(node_modules)/,
+					loaders: [
+						'babel?presets[]=stage-0,presets[]=react,presets[]=es2015'
+					]
+				}
+			],
+			preLoaders: [
+				{ test: /\.jsx?$/, loader: 'eslint', exclude: /node_modules/ },
+				{
+					test: /index\.jsx$/,
+					loader: 'string-replace',
+					query: {
+						multiple: [
+							 { search: '@plugin_bundle', replace: manifest.bundle }
+						]
+					}
+				}
+			]
+		},
+		cssLoader: {
+			modules: false,
+			importLoaders: 1,
+			sourceMap: true
+		},
+		eslint: {
+			failOnWarning: false,
+			failOnError: true
+		},
+		plugins: [
+			new ExtractTextPlugin("style.css"),
+			new webpack.DefinePlugin({
+				'process.env': {
+					'NODE_ENV': JSON.stringify('development')
+				}
+			}),
+			new CopyWebpackPlugin([
+					{ from: 'manifest.json', to: '.' }
+			])
+		]
+}
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -7,89 +7,91 @@
 |
 */
 
-const path = require('path');
-const webpack = require('webpack');
-const manifest = require('./manifest.json');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const autoprefixer = require('autoprefixer');
+const path = require('path')
+const webpack = require('webpack')
+const manifest = require('./manifest.json')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const autoprefixer = require('autoprefixer')
 
 console.log("\n ----------------------------")
 console.log(" Plugin production build ")
 console.log(" ----------------------------\n")
 
 module.exports = {
-    entry: "./src/js/main.js",
-    output: {
-        filename: "index.js",
-        path: "dist",
-    },
-    postcss: [
-        autoprefixer({
-            browsers: ['last 2 versions']
-        })
-    ],
-    module: {
-        loaders: [
-          {
-            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-            loader: 'url-loader?limit=100000'
-          },
-          {
-              test: /\.scss$/,
-              loader: ExtractTextPlugin.extract('style', 'css!postcss!sass')
-          },
-          {
-              test: /\.jsx?$/,
-              exclude: /(node_modules)/,
-              loaders: [
-                  'babel?presets[]=stage-0,presets[]=react,presets[]=es2015'
-              ]
-          }
-        ],
-        preLoaders: [
-          { test: /\.jsx?$/, loader: 'eslint', exclude: /node_modules/ },
-          {
-            test: /index\.jsx$/,
-            loader: 'string-replace',
-            query: {
-              multiple: [
-                 { search: '@plugin_bundle', replace: manifest.bundle }
-              ]
-            }
-          }
-        ]
-    },
-    cssLoader: {
-        // True enables local scoped css
-        modules: false,
-        // Which loaders should be applied to @imported resources (How many after the css loader)
-        importLoaders: 1,
-        sourceMap: false
-    },
-    eslint: {
-        failOnWarning: false,
-        failOnError: true
-    },
-    plugins: [
-        new ExtractTextPlugin("style.css"),
-        new webpack.DefinePlugin({
-          'process.env': {
-            'NODE_ENV': JSON.stringify('production')
-          }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-          minimize: true,
-          sourceMap: false,
-          output: {
-            comments: false
-          },
-          compress: {
-            warnings: false
-          }
-        }),
-        new CopyWebpackPlugin([
-            { from: 'manifest.json', to: '.' }
-        ])
-    ]
-};
+		entry: "./src/js/main.js",
+		output: {
+			filename: "index.js",
+			path: "dist",
+		},
+		externals: [
+			"Dashboard",
+			"React"
+		],
+		postcss: [
+			autoprefixer({
+				browsers: ['last 2 versions']
+			})
+		],
+		module: {
+			loaders: [
+				{
+					test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+					loader: 'url-loader?limit=100000'
+				},
+				{
+					test: /\.scss$/,
+					loader: ExtractTextPlugin.extract('style', 'css!postcss!sass')
+				},
+				{
+					test: /\.jsx?$/,
+					exclude: /(node_modules)/,
+					loaders: [
+						'babel?presets[]=stage-0,presets[]=react,presets[]=es2015'
+					]
+				}
+			],
+			preLoaders: [
+				{ test: /\.jsx?$/, loader: 'eslint', exclude: /node_modules/ },
+				{
+					test: /index\.jsx$/,
+					loader: 'string-replace',
+					query: {
+						multiple: [
+							 { search: '@plugin_bundle', replace: manifest.bundle }
+						]
+					}
+				}
+			]
+		},
+		cssLoader: {
+			modules: false,
+			importLoaders: 1,
+			sourceMap: false
+		},
+		eslint: {
+			failOnWarning: false,
+			failOnError: true
+		},
+		plugins: [
+			new ExtractTextPlugin("style.css"),
+			new webpack.DefinePlugin({
+				'process.env': {
+					'NODE_ENV': JSON.stringify('production')
+				}
+			}),
+			new webpack.optimize.UglifyJsPlugin({
+				minimize: true,
+				sourceMap: false,
+				output: {
+					comments: false
+				},
+				compress: {
+					warnings: false
+				}
+			}),
+			new CopyWebpackPlugin([
+					{ from: 'manifest.json', to: '.' }
+			])
+		]
+}
