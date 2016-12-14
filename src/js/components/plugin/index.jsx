0136cd5153249e4ae1ae9493a3f4bdd3c7565c81
Johan Sundén <johan.sunden@infomaker.se>
2016-12-14 10:53:09 +0100

    Added plugin_bundle_class and comments

--- src/js/components/plugin/index.jsx
+++ src/js/components/plugin/index.jsx
@@ -3,14 +3,16 @@
 | Index.jsx
 |--------------------------------------------------------------------------
 |
-| Welcome to your plugin. Your plugin can have an agent, widget and a application.
+| Welcome to your plugin.
 | Documentation can be found at https://github.com/Infomaker/Dashboard-Plugin/wiki.
+| Report bugs or leave feedback (about plugins or the Dashboard) at https://github.com/Infomaker/Dashboard-Plugin/issues.
 |
 */
 
 ((Dashboard, React) => {
 	/**
 	 * Create an Application by extending the Application class
+	 * Read more about Application (https://github.com/Infomaker/Dashboard-Plugin/wiki/Application)
 	*/
 	class Application extends Dashboard.Application {
 		constructor(props) {
@@ -22,10 +24,12 @@
 		}
 
 		render() {
+			// Get the GUI Library from Dashboard.GUI (https://github.com/Infomaker/Dashboard-Plugin/wiki/GUI-Library)
 			const GUI = Dashboard.GUI
 
 			return (
-				<GUI.Wrapper className="se-infomaker-dashboard-plugin">
+				// Use @plugin_bundle_class and the bundle in the manifest will be used as your class
+				<GUI.Wrapper className="@plugin_bundle_class">
 					<GUI.Title text={this.state.config.pluginTitle || "hello world"}/>
 
 					<GUI.Button text="Open a modal" size="large" onClick={() => this.openModal(Modal)} />
@@ -36,9 +40,12 @@
 
 	/**
 	 * Create an Modal by extending the Modal class
+	 * Read more about Modal (https://github.com/Infomaker/Dashboard-Plugin/wiki/Modal)
 	*/
 	class Modal extends Dashboard.Modal {
 		componentWillMount() {
+
+			// Call setTitle to set the component most upper title.
 			this.props.setTitle("My Modal")
 		}
 
@@ -56,6 +63,7 @@
 
 	/**
 	 * Create a Widget by extending the Widget class
+	 * Read more about Widget (https://github.com/Infomaker/Dashboard-Plugin/wiki/Widget)
 	*/
 	class Widget extends Dashboard.Widget {
 		render() {
@@ -67,6 +75,7 @@
 
 	/**
 	 * Create an Agent by extending the Agent class
+	 * Read more about Agent (https://github.com/Infomaker/Dashboard-Plugin/wiki/Agent)
 	*/
 	class Agent extends Dashboard.Agent {
 		constructor() {
@@ -83,18 +92,28 @@
 		}
 	}
 
+	/**
+	 * Create settings for you plugin by extending the Dashboard.Settings component
+	*/
 	class Settings extends Dashboard.Settings {
+		// Plugin settings will be displayed in the store. These settings will be available for Agent, Widget and Application.
+		plugin() {
+			return <Dashboard.GUI.ConfigInput ref="pluginTitle" />
+		}
+
+		// Application settings will be displayed in application settings mode. These settings will only be available for the application.
 		application() {
 			return <Dashboard.GUI.ConfigInput ref="pluginTitle" />
 		}
 	}
 
 	/**
-	 * Register your plugin
+	 * Register your plugin in the Dashboard.
 	*/
 	Dashboard.register({
 		// Leave this be and it will fetch the data from your manifest file in the build steps
 		bundle: "@plugin_bundle",
+		
 		// Only of of these are actually required. If you are developing a widget, just remove the application and agent.
 		application: Application,
 		widget: Widget,
