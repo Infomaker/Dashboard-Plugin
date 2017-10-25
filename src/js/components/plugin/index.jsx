e0512f276eccfcd742c486578f65be938f0a06dd
Johan Sundén <johan.sunden@infomaker.se>
2017-10-25 15:25:56 +0200

    Added label to input fields

--- src/js/components/plugin/index.jsx
+++ src/js/components/plugin/index.jsx
@@ -98,12 +98,12 @@
 	class Settings extends Dashboard.Settings {
 		// Plugin settings will be displayed in the store. These settings will be available for Agent, Widget and Application.
 		plugin() {
-			return <Dashboard.GUI.ConfigInput ref="pluginTitle" />
+			return <Dashboard.GUI.ConfigInput name="Title" ref="pluginTitle" />
 		}
 
 		// Application settings will be displayed in application settings mode. These settings will only be available for the application.
 		application() {
-			return <Dashboard.GUI.ConfigInput ref="pluginTitle" />
+			return <Dashboard.GUI.ConfigInput name="Title" ref="pluginTitle" />
 		}
 	}
 
