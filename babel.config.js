c81c609af71b884e3afc2e14dc748b6232d376ec
hamzahalhariri <hamzah.alhariri@infomaker.se>
2019-05-17 16:30:48 +0200

    Updated dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,22 @@
-module.exports = function(api) {
-	let presets = [
-		'@babel/preset-env',
-		'@babel/preset-react'
-	]
+module.exports = function (api) {
+    let presets = [
+        '@babel/preset-env',
+        '@babel/preset-react'
+    ]
 
-	let plugins = [
-		"babel-plugin-styled-components"
-	]
+    let plugins = [
+        "babel-plugin-styled-components"
+    ]
 
-	if (api.env('development')){
-		// add development specific presets/plugins here
-		
-	} else if (api.env('production')) {
-		// add production specific presets/plugins here
-	}
+    if (api.env('development')) {
+        // add development specific presets/plugins here
 
-	return {
-		presets,
-		plugins
-	}
+    } else if (api.env('production')) {
+        // add production specific presets/plugins here
+    }
+
+    return {
+        presets,
+        plugins
+    }
 }
